e69f24c68373ff45980c8f1be317f29c14488b31
Ryo Hilmi <ryodm.74@gmail.com>
2022-09-27 20:50:19 +0700

    add basic scene

---
+++ main.js
@@ -0,0 +1,35 @@
+import "./assets/scss/style.scss";
+import * as THREE from "three";
+
+const canvas = document.querySelector("canvas");
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+const light = new THREE.AmbientLight(0x404040); // soft white light
+scene.add(light);
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
