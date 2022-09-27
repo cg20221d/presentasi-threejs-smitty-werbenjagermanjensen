c96be8a39433855528248b7aac7f86383b0d3e14
Ryo Hilmi <ryodm.74@gmail.com>
2022-09-27 22:30:02 +0700

    add objects

--- main.js
+++ main.js
@@ -21,6 +21,23 @@ renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
+// #region Shapes
+const planeGeometry = new THREE.BoxGeometry(30, 0.1, 30);
+const planeMaterial = new THREE.MeshStandardMaterial({ color: 0xaaaaaa });
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+planeMesh.receiveShadow = true;
+planeMesh.castShadow = false;
+planeMesh.position.y = -3;
+scene.add(planeMesh);
+
+const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
+const sphereMaterial = new THREE.MeshStandardMaterial({ color: 0xaaaaaa });
+const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphereMesh.castShadow = true;
+sphereMesh.receiveShadow = true;
+scene.add(sphereMesh);
+
+// #endregion
 
 const light = new THREE.AmbientLight(0x404040); // soft white light
 scene.add(light);
